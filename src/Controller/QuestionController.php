d61a9c61803c25525054b8380c07cde3012c7688
Fill <happyhellakasacred@gmail.com>
2020-05-30 22:45:03 +0700

    add profiler-pack and debug-pack, add CSS and generate URLs

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -11,15 +11,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class QuestionController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {
-        return new Response('What a bewitching controller we have conjured!');
+        return $this->render('question/homepage.html.twig');
     }
 
     /**
-     * @Route("/questions/{slug}")
+     * @Route("/questions/{slug}", name="app_question_show")
      */
     public function show($slug)
     {
@@ -28,6 +28,8 @@ class QuestionController extends AbstractController
           'Honestly, I like furry shoes better than MY cat',
           'Maybee... try saying the spell backwards?',
         ];
+
+        dump($this);
 
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
